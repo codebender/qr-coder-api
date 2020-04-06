ddacad56c91c5254a5be9d8c58da58472dcfb55c
Matthew Bender <benderm@gmail.com>
2020-04-06 11:57:20 -0600

    qr code encoding

--- service/service.go
+++ service/service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-kit/kit/log"
+	qrcode "github.com/skip2/go-qrcode"
 )
 
 // Service describes a service that generates QR Code images.
@@ -30,5 +32,24 @@ type service struct{}
 
 // Generate encodes the input data and returns the
 func (s service) Generate(ctx context.Context, data string) ([]byte, error) {
-	return nil, nil
+	err := validateData(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	png, err := qrcode.Encode(data, qrcode.Medium, 256)
+	if err != nil {
+		return nil, fmt.Errorf("An error occured while endcoding the data, err=%w", err)
+	}
+
+	return png, nil
+}
+
+func validateData(data string) error {
+	if data == "" {
+		return fmt.Errorf("Data is required")
+	}
+
+	return nil
 }
--- service/service_test.go
+++ service/service_test.go
@@ -1,10 +1,13 @@
 package service_test
 
 import (
+	"context"
 	"testing"
 
+	"github.com/codebender/qrcode-api/service"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	qrcode "github.com/skip2/go-qrcode"
 )
 
 func TestService(t *testing.T) {
@@ -13,5 +16,33 @@ func TestService(t *testing.T) {
 }
 
 var _ = Describe("QR Code Service", func() {
+	Describe("Generate", func() {
+		var (
+			subject service.Service
+			ctx     context.Context
+		)
 
+		BeforeEach(func() {
+			subject = service.NewService()
+			ctx = context.Background()
+		})
+
+		It("returns a generated qr code png's bytes", func() {
+			testData := "testing123"
+			qrCode, err := subject.Generate(ctx, testData)
+			Expect(err).ToNot(HaveOccurred())
+
+			expectedPNG, err := qrcode.Encode(testData, qrcode.Medium, 256)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(qrCode).To(Equal(expectedPNG))
+
+		})
+
+		It("returns an error when no data is give to encode", func() {
+			_, err := subject.Generate(ctx, "")
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Data is required"))
+		})
+	})
 })
